2270df8e393d2d9af0b84a6cfa148968f9c9e6e6
oivoodoo <oivoodoo@oivoodoo-laptop.(none)>
2010-02-21 15:32:20 +0200

    Add crudify plugin

---
+++ lib/voodoo/voo_crudify.rb
@@ -0,0 +1,62 @@
+module Voodoo
+  module BasePage
+    module ControllerClassMethods
+      def self.included(base)
+        base.extend(self)
+      end
+
+      def voo_crudify(model, options = {})
+        module_eval %(
+          before_filter :find_#{model}, :only => %w(edit update show destroy)
+
+          def index
+            @#{model.pluralize} = #{model.camelize}.paginate :page => params[:page], :select => 'id, title, updated_at, position, #{options[:container]}_id'
+          end
+
+          def show
+          end
+
+          def new
+            @#{model} = #{model.camelize}.new
+          end
+
+          def edit
+          end
+
+          def create
+            @#{model} = #{model.camelize}.new(params[:#{model}])
+            @#{model}.#{options[:container]} = #{options[:container].camelize}.first(:select => 'id')
+
+            if @#{model}.save
+              flash[:notice] = '#{model.camelize} was successfully created.'
+              redirect_to [:admin, @#{model}]
+            else
+              render :action => 'new'
+            end
+          end
+
+          def update
+            if @#{model}.update_attributes(params[:#{model}])
+              flash[:notice] = '#{model.camelize} was successfully updated.'
+              redirect_to [:admin, @#{model}]
+            else
+              render :action => 'edit'
+            end
+          end
+
+          def destroy
+            @#{model}.destroy
+            redirect_to admin_#{model.pluralize}_url
+          end
+
+          private
+
+          def find_#{model}
+            @#{model} = #{model.camelize}.find(params[:id])
+          end
+        )
+      end
+    end
+  end
+end
+
--- lib/voodoo/voo_move_item.rb
+++ lib/voodoo/voo_move_item.rb
@@ -5,19 +5,38 @@ module Voodoo
         base.extend(self)
       end
 
+      @@config = {:column => "position"}
+
       def voo_move_item(parent, model, options = {})
-        define_method "move_#{model}" do 
-          Object.const_get(model.to_s.camelize.to_sym).find(params[:id]).send(params[:method])
-          instance_variable_set("@#{model.to_s.pluralize}", Object.const_get(parent.to_s.camelize.to_sym).first.send("#{model.to_s.pluralize.to_sym}"))
-          respond_to do |wants|
-            wants.html {
-              unless options.include?(:template)
-                render :partial => "#{model.to_s.pluralize}/#{model.to_s}", :collection => eval("@#{model.to_s.pluralize}")
-              else
-                render :partial => "#{options[:template]}", :collection => eval("@#{model.to_s.pluralize}")
-              end
-            }
+        @@config[:column] ||= options[:column]
+
+        define_method "move_#{model}" do
+          item = Object.const_get(model.to_s.camelize.to_sym).find(params[:id], :select => options[:select])
+          if not (item.first? and params[:method] == "higher") and not (item.last? and params[:method] == "lower")
+            item.send("move_#{params[:method]}".to_sym)
+            if options.include?(:paginate)
+              instance_variable_set("@#{model.to_s.pluralize}", Object.const_get(model.to_s.camelize.to_sym).paginate(:page => params[:page], :per_page => options[:paginate],
+                    :conditions => ["#{parent.to_s}_id = ?", Object.const_get(parent.to_s.camelize.to_sym).first.id], :order => config[:column], :select => options[:select]))
+            else
+              instance_variable_set("@#{model.to_s.pluralize}", Object.const_get(parent.to_s.camelize.to_sym).first.send("#{model.to_s.pluralize.to_sym}"))
+            end
+            next_item = item.send("#{inverse_method(params[:method])}_item".to_sym)
           end
+          respond_to do |wants|
+              wants.js {
+                  unless next_item.nil?
+                    render :json  => {:item1 => {:id => item.id},
+                                      :item2 => {:id => next_item.id}}
+                  else
+                    render :json => "nothing to do"
+                  end
+              }
+          end
+        end
+
+        def inverse_method(method)
+          return "higher" if method == "lower"
+          return "lower" if method == "higher"
         end
       end
     end
